ac48d25618c3d8bd4563c750dce862f2903e6db6
이연수 <yeonsoo@Yeonsoo-MacBookAir.local>
2023-03-17 17:49:51 +0900

    in progress

--- Assignment1/in_class_exercise.py
+++ Assignment1/in_class_exercise.py
@@ -1,17 +1,19 @@
 ##### Online Retail II Data Analysis on UCI Machine Learning Repository #####
 
 import datetime as dt
+import matplotlib.pyplot as plt
 import os
 import pandas as pd
 from tabulate import tabulate
 
 ### Study the dataset
 # read excel dataset with two sheets 
+'''
 df0910 = pd.read_excel(os.path.join("data", "online_retail_II.xlsx"), sheet_name=0) # 2009-2010
 df1011 = pd.read_excel(os.path.join("data", "online_retail_II.xlsx"), sheet_name=1) # 2010-2011
 df = pd.concat([df0910, df1011], axis=0) # merge data 
 df.to_csv(path_or_buf=os.path.join("data", "online_retail_II.csv")) # save the df as a csv file
-
+'''
 ### Data preprocessing 1
 df = pd.read_csv(os.path.join("data", "online_retail_II.csv")) # read the csv file
 print(df.dtypes) # checking data types
@@ -35,13 +37,31 @@ df_val['Minute'] = df_val['InvoiceDate'].dt.minute
 df_val['Second'] = df_val['InvoiceDate'].dt.second
 df_val['Weekday'] = df_val['InvoiceDate'].dt.weekday # 0 Monday - 6 Sunday
 df_val = df_val.astype({"Year": "Int32", "Month": "Int32", "Day": "Int32", "Hour": "Int32", "Minute": "Int32", "Second": "Int32", "Weekday": "Int32"})
-print(df_val.dtypes)
-print(df_val.head())
 # creating a variable to measure recency of transactions
 present = dt.datetime(2011,12,31)
 df_val['Recency'] = present - df_val.groupby(['Customer ID'])['InvoiceDate'].transform(max)
 
 ### Data preprocessing 3
-df_val.describe() # summary statistics
+print(df_val.describe()) # summary statistics
+df_val.drop(df_val[df_val['Quantity'] < 0].index, inplace=True) # clean negative quantities 
+df_val.drop(df_val[df_val['Price'] == 0].index, inplace=True)  # clean zero price
 
-# clean negative quantities and zero price
+### Exploratory Data Analysis (EDA)
+# unique invoices (transactions), products, and customers & their distributions
+df_val = df_val.astype({"Customer ID": "O"})
+print(df_val.describe(include=['O'])) # 36969 unique invoices, 3631 unique products, 5878 unique customers
+invoice_cnt = df_val.groupby('Invoice').count()
+print(invoice_cnt['StockCode'].describe()) # descriptive statistics on number of products ordered per transaction
+invoice_cnt.hist(column='StockCode', bins=50)  # histogram of number of products ordered per transaction
+plt.show()
+product_cnt = df_val.groupby('StockCode').count()
+print(product_cnt['Quantity'].describe()) # descriptive statistics on how many times each product is ordered
+product_cnt.hist(column='Quantity', bins=50)  # histogram of number of orders per product
+plt.show()
+customer_cnt = df_val.groupby('Customer ID').count()
+print(customer_cnt['Quantity'].describe()) # descriptive statistics on number of purchases per customer (at product level)
+customer_cnt.hist(column='Quantity', bins=50)  # histogram of number of purchases per customer (at product level)
+plt.show()
+# who buys what, how often, how much, and when?
+# composition of baskets (invoices)
+# graphical presentations
