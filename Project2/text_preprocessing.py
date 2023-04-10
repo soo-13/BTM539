83b2b71412ffce1969f7f61339b0f50f54e5a32f
이연수 <yeonsoo@Yeonsoo-MacBookAir.local>
2023-04-11 00:23:07 +0900

    text preprocessing

--- Project2/text_preprocessing.py
+++ Project2/text_preprocessing.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as pit
+import matplotlib.pyplot as plt
 import os
 import pandas as pd
 import spacy
@@ -47,6 +47,7 @@ print(abst_stem.shape)
 
 ### 3 n-grams & Infrequently used terms
 vectorizer1 = CountVectorizer(ngram_range=(1,3), min_df=0.01) # Initialize CountVectorizer with 1,2,3-gram and min_df of 1%
+# for unstemmed data
 vector = vectorizer1.fit_transform(abst).toarray() # Fit the vectorizer to your text data
 print(vector.shape)
 print(len(vectorizer1.vocabulary_))
@@ -57,9 +58,31 @@ print(vector_stem.shape)
 
 ### compare with stemming vs. without stemming
 wostm = list(vectorizer1.vocabulary_.keys()) # without stemming
-print(sorted(wostm))
+#print(sorted(wostm))
 wstm = list(vectorizer2.vocabulary_.keys()) # with stemming
-print(sorted(wstm))
+#print(sorted(wstm))
 
-### visualize word counts per journal
-words_df = pd.DataFrame(vector) # without stemming
+### visualize word counts per journal 
+journal_title = {'MANAGE SCI': 'MS', 'ORGAN SCI': 'OS', 'ACAD MANAGE J': 'AMJ', 'STRATEGIC MANAGE J': 'SMJ'}
+def visualize_word_counts_per_journal(wc, title):
+    colors = ['blue', 'green', 'red', 'purple']
+    plt.clf()
+    fig, axs = plt.subplots(2,2, figsize=(12,8))
+    for i, ax in enumerate(axs.flat):
+        journal = df['Jabb'].unique()[i]
+        tmp = wc[df['Jabb']==journal]
+        ax.hist(tmp, bins=30, alpha=0.5, color=colors[i])
+        ax.set_title(journal_title[journal])
+        ax.set_xlabel('Word count')
+        ax.set_ylabel('frequency')
+        ax.xaxis.set_label_coords(1.1, -0.05) # move xlabel to the right side of x-axis
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+    plt.suptitle(title, fontweight='bold')
+    plt.show()
+       
+# after preprocessing
+words_count_after = pd.Series(np.sum(vector, axis=1)) # word count for each article as pd Series
+visualize_word_counts_per_journal(words_count_after, 'Word count distribution by journal after preprocessing')
+words_count_before = df['Abst'].apply(lambda x: len(x.split()))
+visualize_word_counts_per_journal(words_count_before, 'Word count distribution by journal before preprocessing')
+
